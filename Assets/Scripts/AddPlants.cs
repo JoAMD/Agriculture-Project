a39aaebc51ca2fbfc8b65799c6f8c9929f8e642c
JoAMD <joasus12345@gmail.com>
2019-04-12 12:44:41 +0530

    First Commit

---
+++ Assets/Scripts/AddPlants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlants : MonoBehaviour
+{
+    public GameObject plant;
+    public float nextTime = 0f;
+    public Vector3 position = new Vector3(0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextTime)
+        {
+            Instantiate(plant, position, Quaternion.identity, transform);
+            nextTime = Time.time + 1f;
+        }
+    }
+}
